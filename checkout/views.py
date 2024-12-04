adf0b6504179d0ad9223276f519b2be8f43c6412
Felix Idén <ouc98oec@students.codeinstitute.net>
2024-12-04 15:06:58 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
@@ -14,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51QSJOyDYzicqtyurN9K8RX8LJVBqMKq9eUIlMmeNs6yrRcn194RfepAIUYH4hLmENi1a0NXvXoXKQIbqJGnafbJR00nETLTe4Q',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
